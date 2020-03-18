04df2450873235d0c0f77166279055387d7e9d24
jiangxch <jiangxch8355@fxiaoke.com>
2020-03-18 18:24:27 +0800

    init

---
+++ src/main/java/com/sanjin/jenkins/demo/Main.java
@@ -0,0 +1,14 @@
+package com.sanjin.jenkins.demo;
+
+/**
+ * @author: jiangxch
+ * @date: 2019/12/31 12:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println("hello world");
+        }
+
+    }
+}
